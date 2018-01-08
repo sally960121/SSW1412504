d1e3c807d2f82edd8ad58c10e17826b6ba5df336
SEOWON96 <sallysw12121212@gmail.com>
2018-01-08 23:27:46 +0900

    choose

--- SSW1412504/src/MyCalc.java
+++ SSW1412504/src/MyCalc.java
@@ -1,4 +1,34 @@
+import java.util.Scanner;
 
 public class MyCalc {
 
+	public static void main(String[] args) {
+		
+		choose();
+		
+		
+	}
+	static void choose() {
+		System.out.println("ID: 1412504");
+		System.out.println("Name: ¼Û¼­¿ø");
+		
+		System.out.println("1. Add two numbers");
+		System.out.println("2. Subtract two numbers");
+		System.out.println("3. Multiply two numbers");
+		System.out.println("4. Quit");
+		
+		
+		Scanner in = new Scanner(System.in);
+		
+		
+		System.out.println("choose> ");
+		int choice = in.nextInt();
+		
+		System.out.println(">x: ");
+		int number1 = in.nextInt();
+		System.out.println(">y: ");
+		int number2 = in.nextInt();
+	
+	}
+	
 }
